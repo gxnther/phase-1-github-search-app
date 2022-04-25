1325de3780ee833bf405a7f009158d1c1ed25dc6
Gunther <gunt.valentine@gmail.com>
2022-04-25 18:14:26 -0400

    Done.

---
+++ js/index.js
@@ -0,0 +1,11 @@
+var form = document.getElementById("github-form")
+
+
+form.addEventListener(`submit`, function(e){
+        e.preventDefault()
+        var search = document.getElementById("search").value
+        console.log(search)
+    })    
+    
+
+
