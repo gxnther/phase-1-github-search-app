ba1501f93f4fc241eefb0ad3259933dd88609503
Gunther <gunt.valentine@gmail.com>
2022-04-26 11:37:59 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -1,11 +1,23 @@
-var form = document.getElementById("github-form")
+let form = document.querySelector("#github-form")
 
 
 form.addEventListener(`submit`, function(e){
         e.preventDefault()
         var search = document.getElementById("search").value
-        console.log(search)
-    })    
+    fetch("https://api.github.com/users/"+search)
+    .then((resp) => resp.json())
+    .then((data) => {
+        console.log(data)
+        document.getElementById("user-list").innerHTML = `
+        <p>Username: ${data.login}</p>
+        <img id="repo" src ="${data.avatar_url}"/>
+        `
+        let repoURL = document.getElementById("repo")
+        console.log(repoURL)
+    })
+})    
     
+    
+
 
 
